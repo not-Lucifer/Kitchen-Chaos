8151564f1ef30bf4744baa151779e2567fcca248
Aman Singh <amansingh.makemeup@gmail.com>
2026-06-06 12:26:11 +0530

    Adding Delivery Counter and Shader Graph

---
+++ Assets/Scripts/PlateIconsSingleUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlateIconsSingleUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    public void SetKitchenObjectSO(KitchenObjectsSO kitchenObjectsSO)
+    {
+        image.sprite = kitchenObjectsSO.sprite;
+    }
+    
+
+}
